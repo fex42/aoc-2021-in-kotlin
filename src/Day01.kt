927393a6c243f01849f27914823de04f98753088
Felix Na <felix.na70@gmail.com>
2021-12-04 13:32:12 +0100

    solved day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var counter = 0
+        var lastValue = Int.MAX_VALUE
+        input.forEach {
+            if (lastValue < it) {
+                counter++
+            }
+            lastValue = it
+        }
+        return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var counter = 0
+        var lastValue = Int.MAX_VALUE
+
+        (0..input.size-3).forEach {
+            val windowSum = input.slice(it..it+2).sum()
+            if (lastValue < windowSum) {
+                counter++
+            }
+            lastValue = windowSum
+        }
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
